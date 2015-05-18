8ccc608717384894d6a8076b49149ac5c27fb845
Devin Higgins <devinhiggins@gmail.com>
2015-05-17 20:12:39 -0400

    Updated docstrings and README.

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 Discogs API
 ===========
 
-Scripts for accessing discogs API and processing data.
---------------------------------------------
+Scripts for accessing discogs API and processing data that's returned.
+
+For more information about the Discogs API:
 
 [Discogs API documentation](http://www.discogs.com/developers/)
+
+Non built-in libraries used by this module:
+
 [Networkx Library](http://networkx.github.io/documentation/latest/install.html)
 
-Scripts currently in place to return a user's collection; compiles styles for all releases; build network graph and output results in gephi or D3 format.
+[FormatObject](https://git.lib.msu.edu/higgi135/formatobject/tree/master)
 
-Clone repository and change into its `py` directory:
+
+Getting Started
+--------------------
+
+Scripts currently in place to return a user's collection; compiles "styles" for all releases; build network graph and output results in gephi or D3 format.
+
+To work with this library, first clone the repository and change into its `py` directory:
 
 	cd discogs/py
 
@@ -18,13 +28,17 @@ Open Python (or iPython) shell:
     from discogs import DiscogsData
     dd = DiscogsData()
 
-Run `CollectionStyleGraph` to gather data about styles for every release in a given collection. Supply a discogs username and the ID for a folder. Folder `0` contains all releases in a user's collection. All other folders require additional authentication to access and aren't currently functional.
+Run `CollectionStyleGraph` to gather data about styles for every release in a given collection. Run `GetFullCollection` to get data for all releases. Supply a discogs username and the ID for a folder. Folder `0` contains all releases in a user's collection. All other folders require additional authentication to access and aren't currently functional.
 
     data = dd.CollectionStyleGraph(username="[discogs_user]", folder_id="0")
 
+or
+
+    data = dd.GetFullCollection(username="[discogs_user]", folder_id="0")
+
 To instead load locally-stored data, supply a path to a JSON file in the appropriate format (a list of lists), when initiating the class.
 
-    dd = DiscogsData("/Users/devinhiggins/Projects/discogs/data/joshua-styles_by_release.json")
+    dd = DiscogsData(path_to_data="/Users/devinhiggins/Projects/discogs/data/joshua-styles_by_release.json")
     data = dd.CollectionStyleGraph()
 
 To output the data in a format suitable for use in Gephi or a D3 network graph:
--- discogs.py
+++ discogs.py
@@ -8,19 +8,21 @@ from urlparse import urljoin
 import networkx as nx
 from pprint import pprint
 from itertools import combinations
-from pyjson import FormatObject
+from formatobject.formatobject import FormatObject
 import networkx as nx
 from networkx.readwrite import json_graph
 
 
 class DiscogsApi():
-
+    """Class for interacting with Discogs API."""
+    
     def __init__(self):
-        
+        """Establish base API url and provide arbitrary user agent name."""    
         self.base_url = "https://api.discogs.com"
+        self.user_agent = "DevinHigginsMSULibraries/0.1"
 
     def _OpenUrl(self):
-
+        """Function to make all calls to the API."""
         data = ""
         if self.params:
             data = urllib.urlencode(self.params)
@@ -28,24 +30,41 @@ class DiscogsApi():
         else:
             search_url = self.url
         request = urllib2.Request(search_url)
+        request.add_header('User-Agent', self.user_agent)
         response = urllib2.urlopen(request)
         json_response = json.load(response)
-        time.sleep(1.1)
+        time.sleep(2)
         return json_response
 
     def _AddParam(self, key, value):
+        """
+        Add parameter to the header of the request sent to the API.
+
+        Positional arguments:
+        key (str) -- key to use in header, e.g. 'User-Agent'
+        value (str) -- value to associated with the given key.
+        """
 
         self.params[key] = value
 
     def _ClearParams(self):
+        """Clear all current header parameters."""
 
         self.params = {}
 
     def GetCollection(self, username, folder_id="0", per_page=100):
         """
-        Use discogs username and folder to return release data from given folder; 
+        Use discogs username to return release data from given folder.
         Most folders require authentication for access. Folder "0" is the "all" folder.
+
+        Positional arguments:
+        username (str) -- Any valid discogs user name; no authentication required.
+
+        Keyword arguments:
+        folder_id (str) -- The folder containing all releases (0) is the only one supported.
+        per_page (int) -- The number of results to return per request. Max value is 100.
         """
+
         self._ClearParams()
         self._AddParam("per_page", per_page)
 
@@ -67,7 +86,10 @@ class DiscogsApi():
 
     def GetRelease(self, release_id):
         """
-        Use release ID to return release data
+        Use release ID to return release data.
+
+        Positional arguments:
+        release_id (str) -- The numerical id for a given discogs release.
         """
         self._ClearParams()
         self.release_extension = "/releases/{release_id}".format(release_id=release_id)
@@ -78,16 +100,23 @@ class DiscogsApi():
 
     def _BuildUrl(self, url_extension):
         """
-        Combine base url and search extension
+        Combine base url and search extension.
+
+        Positional arguments:
+        url_extension (str) -- query string to append to the base url.
         """
         return urljoin(self.base_url, url_extension)
 
 
 class DiscogsData():
+    """Class to process data accessed via the Discogs API."""
     
     def __init__(self, path_to_data=None):
         """
         If no data is provided as an argument here (JSON), data will be collected via API.
+
+        Keyword arguments:
+        path_to_data (str) -- Must be a valid path to a JSON file matching expected formatting.
         """
         
         self.data = None
@@ -98,6 +127,15 @@ class DiscogsData():
             self.data = json_object.LoadJson(self.path_to_data)
 
     def ReturnXmlReleases(self, username="username", folder_id="folder_id", output_path="/"):
+        """
+        Special function to access release data and transform it into XML.
+
+        Keyword arguments:
+        username (str) -- Any valid discogs user name; no authentication required.
+        folder_id (str) -- The folder containing all releases (0) is the only one supported.
+        output_path (str) -- Location to save file(s) to; defaults to current directory.
+        """
+
         self.username = username
         self.folder_id = folder_id
         dapi = DiscogsApi()
@@ -105,17 +143,26 @@ class DiscogsData():
         for i, release in enumerate(self.releases):
             r_id = release["id"]
             release_data = dapi.GetRelease(r_id)
+
             sys.stdout.write("{:=^30}\r".format(i+1))
             sys.stdout.flush()
+            
             a = FormatObject()
             xml = a.DictToXml(release_data)
             full_path = os.path.join(output_path, str(r_id)+".xml")
             a.WriteFile(xml, full_path)
+
             print "Wrote file at {0}".format(full_path)
 
 
-
     def CollectionStyleGraph(self, username="username", folder_id="0"):
+        """
+        Return networkx graph object linking styles that co-occur by release.
+
+        Keyword arguments:
+        username (str) -- Any valid discogs user name; no authentication required.
+        folder_id (str) -- The folder containing all releases (0) is the only one supported.
+        """
         self.username = username
         self.folder_id = folder_id
         if self.data is None:
@@ -125,7 +172,13 @@ class DiscogsData():
 
     def GraphOutput(self, data_type, output_path="co_graph"):
         """
-        Currently support for writing graph data to formats suitable for Gephi and D3 force layout.
+        Write graph data to formats suitable for Gephi and D3 force layout.
+
+        Positional arguments:
+        data_type (str) -- accepted values are 'gephi' and 'd3'.
+
+        Keyword arguments:
+        output_path (str) -- path to write output file (don't include extension).
         """
 
         valid_types = ["gephi", "d3"]
@@ -154,7 +207,15 @@ class DiscogsData():
             json_data.SaveAsJson(data, output_path)
             print "JSON Written to {0}".format(output_path)
 
+
     def GraphCooccurrenceData(self, data):
+        """
+        Format data representing coocurrences into preliminary nodes and edges.
+
+        Positional arguments:
+        data (list) -- list of instances of coocurring styles.
+        """
+
         format_nodes = {}
         format_edges = {}
         for listing in data:
@@ -173,6 +234,14 @@ class DiscogsData():
         return co_graph
 
     def BuildGraphData(self, nodes, edges):
+        """
+        Use preliminary graph data to build networkx graph object.
+        
+        Positional arguments:
+        nodes (dict) -- Count values for nodes
+        edges (dict) -- Count values for edges
+        """
+
         graph = nx.Graph()
         for key in nodes:
             graph.add_node(key, weight=nodes[key])
@@ -184,13 +253,59 @@ class DiscogsData():
         return graph
 
     def StylesDictCounter(self, styles):
+        """
+        Count frequency of individual styles.
+
+        Positional arguments:
+        styles (list) -- list of styles in a given release.
+        """
+
         for s in styles:
             if s in self.styles:
                 self.styles[s] += 1
             else:
                 self.styles[s] = 1
 
+    def GetFullCollection(self, username, folder_id="0"):
+        """
+        Use discogs username to return full release data for storage, from a given folder.
+        Most folders require authentication for access. Folder "0" is the "all" folder.
+
+        Positional arguments:
+        username (str) -- Any valid discogs user name; no authentication required.
+
+        Keyword arguments:
+        folder_id (str) -- The folder containing all releases (0) is the only one supported.
+        """
+
+        self.folder_id = folder_id
+        self.username = username
+        self.all_releases = []
+        
+        dapi = DiscogsApi()
+        self.releases = dapi.GetCollection(self.username, folder_id=self.folder_id)
+        i = 1
+        for release in self.releases:
+            #print release["basic_information"]["artists"][0]["name"]
+            r_id = release["id"]
+            try:
+                release_data = dapi.GetRelease(r_id)
+                self.all_releases.append(release_data)
+
+            except Exception as e:
+                print e
+                release_data = dapi.GetRelease(r_id)
+                self.all_releases.append(release_data)
+
+            sys.stdout.write("{:=^30}\r".format(i))
+            sys.stdout.flush()
+            i += 1
+
+        return self.all_releases
+
+
     def GetCollectionStyles(self):
+        """Get styles associated with each release in a given collection."""
 
         self.styles_by_release = []
         self.styles = {}
@@ -209,8 +324,8 @@ class DiscogsData():
             sys.stdout.write("{:=^30}\r".format(i))
             sys.stdout.flush()
 
-        print "{:=^30}".format("\nStyles by Count")    
-        pprint(self.styles)
+        #print "{:=^30}".format("\nStyles by Count")    
+        #pprint(self.styles)
         
         return self.styles_by_release
 
